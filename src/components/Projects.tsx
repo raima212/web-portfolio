f3e92335be960f97179b4b48eabb217d70824dbe
MelihIlker <melihilker9@gmail.com>
2025-09-11 21:55:53 +0300

    fix modal z-index

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -282,7 +282,7 @@ const Projects = () => {
         {/* Project Modal */}
         {selectedProject && (
           <div 
-            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
+            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-[60]"
             onClick={closeModal}
             data-modal="true"
           >
